36247b886797e938885245714d5c7227e0c04af7
malf@innoventcorp.com <19920603Ybb>
2021-08-05 14:27:49 +0800

    upload to box general date range transform excel

--- Resource/DelHeader.py
+++ Resource/DelHeader.py
@@ -2,25 +2,37 @@ import os
 
 import pandas as pd
 
+price_list_dict = {
+    'CN': '50                  China',
+    'HK': '50                  China',
+    'TW': '59                  Taiwan'
+}
 
-def transform_htm_2_xls(htm_path: str, xls_path: str):
+dt_type_dict = {
+    4: float,
+    6: float
+}
+
+
+def format_dt_type(df):
+    for k, v in dt_type_dict.items():
+        df[k] = df[k].head(5).append(df[k].tail(-5).astype(v))
+
+
+def transform_htm_2_xls(htm_path: str, xls_path: str, price_list_name: str):
     html_data_list = pd.read_html(open(htm_path))
-    table_data, table_header = list(), list()
+    table_data = [
+        ['Price List', None, None, price_list_dict.get(price_list_name)],
+        [], [],
+        [None, 'CnTy', 'Material', None, ' Amount', 'Unit', 'per', 'UoM', 'Valid From', 'Valid to', 'D'],
+        []
+    ]
     for html_data in html_data_list:
-        table_header = list(list(html_data.values)[0])
         for row_data in list(html_data.values)[1:]:
-            table_data.append(list(row_data))
+            table_data.append([None, *list(row_data)[:2], None, *list(row_data)[2:]])
     df = pd.DataFrame(table_data)
-    df.columns = table_header
+    format_dt_type(df)
     ew = pd.ExcelWriter(xls_path)
-    df.to_excel(ew, index=False)
+    df.to_excel(ew, index=False, header=False)
     ew.save()
-
-
-def transform_folder(file_path: str):
-    [transform_htm_2_xls(os.path.join(file_path, file), os.path.join(file_path, file.replace('htm', 'xls'))) for file in
-     os.listdir(file_path) if str.endswith(file, 'htm')]
-
-
-if __name__ == '__main__':
-    transform_folder(r'C:\Users\WWan95\Desktop')
+    os.remove(htm_path)
